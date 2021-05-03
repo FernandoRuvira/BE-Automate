67a48f168fe2f933f66a89d10d7587a0b0dde895
hjdrakkar <hjdrakkar@gmail.com>
2021-05-03 00:26:50 -0500

    Tiempo de espera promedio

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Carbon;
 
 class Ticket extends Model
 {
@@ -17,5 +19,28 @@ class Ticket extends Model
     public function reason()
     {
         return $this->hasOne(Reason::class, 'id', 'reason_id');
+    }
+
+    public function averageWaitingTime()
+    {
+        $avg = DB::table('tickets_average')
+            ->where('reason_id', $this->reason_id)
+            ->whereDate('created_at', Carbon::today())
+            ->first();
+
+        return $avg->average;
+    }
+
+    public function positionInLine()
+    {
+        $turn = DB::table('tickets')
+            ->where([
+                ['reason_id', $this->reason_id],
+                ['status', 'W'],
+            ])
+            ->whereDate('created_at', Carbon::today())
+            ->count();
+
+        return $turn;
     }
 }
